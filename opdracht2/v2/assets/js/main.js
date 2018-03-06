36611259fc33300e47aee1f9fc880933fa1c58c1
BrianJakobs <31827219+BrianJakobs@users.noreply.github.com>
2018-03-06 16:25:10 +0100

    Uitleg en bronvermelding bij stukje code
    Er is een uitleg toegevoegd aan JS en een vermelding naar een filmpje.

--- opdracht2/v2/assets/js/main.js
+++ opdracht2/v2/assets/js/main.js
@@ -7,6 +7,8 @@ var sliderFotos = document.querySelectorAll(".slide"),
     sleepTekst = document.querySelector("#sleep .tekst"),
     workTekst = document.querySelector("#work .tekst");
 
+//Uitleg van het toegepaste principe voor de foto slider: https://www.youtube.com/watch?v=7ZO2RTMNSAY als bron gebruikt.
+
 // Haal alle foto's weg
 function reset() {
     "use strict";
@@ -17,9 +19,10 @@ function reset() {
 }
 
 // Standaard foto
-function startSlide() {
+function Beginfoto() {
     "use strict";
     reset();
+    //sliderFotos is een array, dus 0 is het eerste item
     sliderFotos[0].classList.toggle("show");
 }
 
@@ -27,27 +30,32 @@ function startSlide() {
 function slideLinks() {
     "use strict";
     reset();
+    //het vorige item in de array wordt zichtbaar gemaakt en dat wordt het nieuwe huidige item.
     sliderFotos[huidig - 1].classList.toggle("show");
     huidig--;
+}
+
+function motionLeft() {
+    if (huidig === 0) {
+        //als het huidige item 0 is, moet de laatste afbeelding in de array weer getoont worden zodat hij kan blijven draaien.
+        huidig = sliderFotos.length;
+    }
+    slideLinks();
 }
 
 // Volgende foto
 function slideRechts() {
     "use strict";
     reset();
+    //het volgende item in de array wordt zichtbaar gemaakt en dat wordt het nieuwe huidige item. 
     sliderFotos[huidig + 1].classList.toggle("show");
     huidig++;
 }
 
-function motionLeft() {
-    if (huidig === 0) {
-        huidig = sliderFotos.length;
-    }
-    slideLinks();
-}
-
 function motionRechts() {
+    //Dit is het laatste item in de array. Deze heeft dan een 'huidig' van 2, die moet dan dus weer 0 worden. 
     if (huidig === sliderFotos.length - 1) {
+        //Deze wordt op -1 gezet zodat als er weer 1 bij komt, door de functie slideRechts, dat het 
         huidig = -1;
     }
     slideRechts();
@@ -59,16 +67,15 @@ arrowLeft.addEventListener("click", motionLeft);
 // Rechter pijl geklikt
 arrowRight.addEventListener("click", motionRechts);
 
-startSlide();
+Beginfoto();
 
 document.onkeydown = function (i) {
     "use strict";
-    i = i || window.event;
     // Linker pijl ingedrukt
-    if (i.keyCode == '37') {
+    if (window.event.keyCode == '37') {
         motionLeft()
         // Rechter pijl ingedrukt
-    } else if (i.keyCode == '39') {
+    } else if (window.event.keyCode == '39') {
         motionRechts()
     }
 }
